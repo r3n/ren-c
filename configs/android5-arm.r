12c6d746988ee354cb11db51e0ca4395017dd9a8
Brian Dickens <hostilefork@gmail.com>
2021-03-06 11:10:21 -0500

    Update Android Builds to use MAKE-FILE

--- configs/android5-arm.r
+++ configs/android5-arm.r
@@ -6,7 +6,11 @@ os-id: 0.13.2
 
 tool-prefix: to-file try get-env "ANDROID_NDK"
 
-gcc-path: tool-prefix/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-gcc
+gcc-path: make-file [
+    (tool-prefix) / toolchains /
+        arm-linux-androideabi-4.9 / prebuilt / linux-x86_64 / bin /
+        arm-linux-androideabi-gcc
+]
 
 toolset: compose [
     gcc (gcc-path)
@@ -14,5 +18,7 @@ toolset: compose [
 ]
 
 ldflags: cflags: reduce [
-    unspaced ["--sysroot=" tool-prefix/platforms/android-19/arch-arm]
+    unspaced ["--sysroot=" make-file [
+        (tool-prefix) / platforms / android-19 / arch-arm
+    ]]
 ]
