1c09a9546f4461de0bb8fe005d61e5670b8093ad
iArnold <arnoldvanh@gmail.com>
2021-05-13 11:42:50 +0200

    Added viewgtk3 config file

---
+++ configs/viewgtk3-config.r
@@ -0,0 +1,104 @@
+REBOL []
+os-id: _
+
+; possible values (words):
+; Execution: Build the target directly without generating a Makefile
+; makefile: Generate a makefile for GNU make
+; nmake: Generate an NMake file for CL
+target: 'execution
+
+extensions: make map! [
+    ; NAME VALUE
+    ; VALUE: one of
+    ; + builtin
+    ; - disabled
+    ; * dynamic
+    ; [modules] dynamic with selected modules
+
+    ; ODBC has dependencies outside of what's available on a stock
+    ; standard C compiler with POSIX or Win32.  Disable this extension by
+    ; default.  (Review the general policy for default inclusions.)
+    ; Clipboard is only implemented in Windows at the moment.
+
+    BMP +
+    Clipboard -
+    Console +
+    Crypt +
+    Debugger +
+    DNS +
+    Event +
+    Filesystem +
+    GIF +
+    Gob +
+    JavaScript -
+    JPG +
+    Library +
+    Locale +
+    Network +
+    ODBC +
+    PNG +
+    Process +
+    Signal -
+    TCC -
+    Time +
+    UUID +
+    UTF +
+    Vector +
+    ViewGTK3 +
+]
+
+rebol-tool: _ ; fallback value if system/options/boot fails
+
+; possible combination:
+; [gcc _ ld _]
+; [cl _ link _]
+toolset: [
+    ;name executable-file-path (_ being default)
+    gcc _
+    ld _
+    strip _
+]
+
+;one of "no", "'asserts", "'symbols" or "'sanitize"
+debug: no
+
+; one of 'no', 1, 2 or 4
+;
+optimize: 2
+
+; one of [c gnu89 gnu99 c99 c11 c++ c++98 c++0x c++11 c++14 c++17]
+;
+standard: 'c
+
+rigorous: no
+
+static: no
+pkg-config: try get-env "PKGCONFIG" ;path to pkg-config, or default
+
+; The original default for WITH-FFI was 'dynamic, but this would cause it to
+; try building the FFI on all configurations.
+;
+with-ffi: no
+
+odbc-requires-ltdl: no
+
+with-tcc: no
+
+; Console API for windows does not exist before vista.
+;
+pre-vista: no
+
+
+git-commit: _
+
+includes: _
+definitions: _
+cflags: _
+libraries: _
+ldflags: _
+
+main: _
+
+top: _
+
+config: _
