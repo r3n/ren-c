dca49aadcbb2f04117ae5a0e03a161c2f3dd1b8a
Brian Dickens <brian@hostilefork.com>
2021-06-03 14:37:40 -0400

    Add missing HERE in %sys-load.r parse
    The goal is that SET-WORD! be used instead of SET VARNAME in native
    PARSE, in the near term (before a unifying merge with UPARSE).
    
    This means previous usages of SET-WORD! to capture the parse position
    have to go through a transitional time where they are changed to
    SET-WORD! followed by "HERE".  A case in %sys-load.r that was missed
    was noticed while fixing another issue.

--- src/mezz/sys-load.r
+++ src/mezz/sys-load.r
@@ -610,7 +610,7 @@ load-module: func [
             let tmp
             parse source [
                 while [
-                    tmp:
+                    tmp: here
                     set name opt set-word!
                     set mod [
                         word! | module! | file! | url! | text! | binary!
@@ -621,7 +621,6 @@ load-module: func [
                         ]
                     )
                 ]
-                end
             ] else [
                 cause-error 'script 'invalid-arg tmp
             ]
